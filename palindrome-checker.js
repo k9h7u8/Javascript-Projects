55f2d44703f528473215b24621f7baafc4afdc7a
Khushi Aggarwal <khushiaggarwal1302@gmail.com>
2022-12-12 19:50:01 +0530

    Palindrome checker

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -16,16 +16,12 @@ function palindrome(str) {
 }
 
 console.log(palindrome("eye"));
-console.log(palindrome("eye"));
 console.log(palindrome("_eye"));
-console.log(palindrome("race car"));
 console.log(palindrome("race car"));
 console.log(palindrome("not a palindrome"));
 console.log(palindrome("A man, a plan, a canal. Panama"));
 console.log(palindrome("never odd or even"));
-console.log(palindrome("nope"));
-console.log(palindrome("almostomla"));
 console.log(palindrome("My age is 0, 0 si ega ym."));
 console.log(palindrome("1 eye for of 1 eye."));
 console.log(palindrome("0_0 (: /-\ :) 0-0"));
-console.log(palindrome("five|\_/|four"));
+console.log(palindrome("five|\_/|six"));
